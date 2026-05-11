eea16e111db304b42626bc219edb1b86b59aabaf
Cedric Martinez <cedric.martinez@cetys.edu.mx>
2026-05-10 22:00:45 -0700

    feat: display of numbers

---
+++ AnalizadorSintactico/MainWindow.axaml.cs
@@ -0,0 +1,31 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace AnalizadorSintactico;
+
+public partial class MainWindow : Window
+{
+    string expression = "";
+
+    public MainWindow() => InitializeComponent();
+
+    void OnInput(object sender, RoutedEventArgs e)
+    {
+        expression += ((Button)sender).Content;
+        Display.Text = expression;
+    }
+
+    void OnOperator(object sender, RoutedEventArgs e)
+    {
+        var symbol = ((Button)sender).Content!.ToString()!;
+        var op = symbol switch { "÷" => "/", "×" => "*", "−" => "-", _ => symbol };
+        expression += op;
+        Display.Text = expression;
+    }
+
+    void OnClear(object sender, RoutedEventArgs e)
+    {
+        expression = "";
+        Display.Text = "0";
+    }
+}

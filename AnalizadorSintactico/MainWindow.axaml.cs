9563ce658145ac523a70e80b6c05fb88e1abaa60
Cedric Martinez <cedric.martinez@cetys.edu.mx>
2026-05-24 19:33:04 -0700

    feat: add delete button

--- AnalizadorSintactico/MainWindow.axaml.cs
+++ AnalizadorSintactico/MainWindow.axaml.cs
@@ -9,23 +9,26 @@ public partial class MainWindow : Window
 
     public MainWindow() => InitializeComponent();
 
-    void OnInput(object sender, RoutedEventArgs e)
-    {
-        expression += ((Button)sender).Content;
-        Display.Text = expression;
+    void OnInput(object sender, RoutedEventArgs e){
+      expression += ((Button)sender).Content;
+      Display.Text = expression;
     }
 
-    void OnOperator(object sender, RoutedEventArgs e)
-    {
-        var symbol = ((Button)sender).Content!.ToString()!;
-        var op = symbol switch { "÷" => "/", "×" => "*", "−" => "-", _ => symbol };
-        expression += op;
-        Display.Text = expression;
+    void OnOperator(object sender, RoutedEventArgs e){
+      var symbol = ((Button)sender).Content!.ToString()!;
+      var op = symbol switch { "÷" => "/", "×" => "*", "−" => "-", _ => symbol };
+      expression += op;
+      Display.Text = expression;
     }
 
-    void OnClear(object sender, RoutedEventArgs e)
-    {
-        expression = "";
-        Display.Text = "0";
+    void OnClear(object sender, RoutedEventArgs e){
+      expression = "";
+      Display.Text = "0";
+    }
+
+    void OnBackspace(object sender, RoutedEventArgs e){
+      if (expression.Length == 0) return;
+      expression = expression[..^1];
+      Display.Text = expression == "" ? "0" : expression;
     }
 }
